08263bcde78670f4c22f3613b19ea92b80397246
DraayZe <lenny.fernet7@gmail.com>
2025-05-02 17:11:25 +0200

    Ajout PassCard

---
+++ components/PassCard.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+defineProps<{
+  pass:{
+    nom: string
+    prix: number
+    duree: number
+    photo: string
+    dates: string[]
+  }
+}>()
+</script>
+
+<template>
+  <div class="relative w-52 flex">
+    <img :src="pass.photo" alt="Pass" class="w-full h-auto object-cover rounded-lg">
+  </div>
+</template>
+
+<style scoped>
+
+</style>
--- pages/billeterie.vue
+++ pages/billeterie.vue
@@ -2,6 +2,7 @@
 import {onMounted, ref} from 'vue'
 import {useRouter} from 'vue-router'
 import Accordeon from '@/components/Accordeon.vue'
+import PassCard from '@/components/PassCard.vue'
 
 const passes = ref([])
 const router = useRouter()
@@ -19,15 +20,23 @@ onMounted(fetchPasses)
 </script>
 
 <template>
-  <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 justify-items-center">
-    <div
-        v-for="pass in passes"
-        :key="pass.id"
-        @click="goToPass(pass.id)"
-    >
-      <img :src="pass.photo" :alt="pass.nom"/>
-    </div>
-  </div>
+<!--  <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 justify-items-center">-->
+<!--    <div-->
+<!--        v-for="pass in passes"-->
+<!--        :key="pass.id"-->
+<!--        @click="goToPass(pass.id)"-->
+<!--    >-->
+<!--    <img :src="pass.photo" :alt="pass.nom"/>-->
+<!--      <p>{{pass.id}}</p>-->
+<!--    </div>-->
+<!--  </div>-->
+  <PassCard
+    v-for="pass in passes"
+    :key="pass.id"
+    :pass="pass"
+    @click.native="goToPass(pass.id)"
+    class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 justify-items-center"
+    />
 
   <div class="bg-[#1E1E1E] ">
     <h2 class="text-white text-center text-7xl font-BevellierBlack py-20">QUESTIONS RÉCURRENTES</h2>
